3c94accdf97a3a696dbd4c9bc6c6eac7f0424acd
unknown <kadiahardi15@gmail.com>
2020-12-26 16:38:53 +0530

    login Signup splash screens

---
+++ app/src/main/java/com/example/daylee/Splash_Screen.kt
@@ -0,0 +1,22 @@
+package com.example.daylee
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash_Screen : AppCompatActivity() {
+
+    private val SPLASH_TIME : Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash__screen)
+
+        Handler().postDelayed( {
+            startActivity(Intent (this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+
+    }
+}
