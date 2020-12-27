2c23fc5167212427a9d6657ed76720bbd85be2e2
JyotiJauhari <jyotijauhari222@gmail.com>
2020-12-27 13:34:13 +0530

    Added Dashboard activity and AddNotes activity

---
+++ app/src/main/java/com/example/daylee/Dashboard.kt
@@ -0,0 +1,30 @@
+package com.example.daylee
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class Dashboard : AppCompatActivity() {
+    private val newAddNoteRequestCode = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        val tasks = Task.getRandomTask(50)
+        val taskadapter = TaskListAdapter(tasks)
+        val rvTask = findViewById<RecyclerView>(R.id.rv_tasks)
+
+        rvTask.layoutManager = LinearLayoutManager(this)
+        rvTask.adapter = taskadapter
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            intent = Intent(applicationContext, AddNote::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
