9f052f852a257efffe26a2d3c5cfe6170d07c1f2
JyotiJauhari <jyotijauhari222@gmail.com>
2020-12-31 14:16:48 +0530

    Validations added

--- app/src/main/java/com/example/daylee/ui/RegisterFragment.kt
+++ app/src/main/java/com/example/daylee/ui/RegisterFragment.kt
@@ -13,6 +13,10 @@ import com.example.daylee.ui.MainActivity
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
 
+    lateinit var name: EditText
+    lateinit var email: EditText
+    lateinit var password: EditText
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
@@ -29,18 +33,50 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
             fragmentTransaction.commit()
         }
 
-        val name = v.findViewById<EditText>(R.id.etName)
-        val email = v.findViewById<EditText>(R.id.etEmail)
-        val pswrd = v.findViewById<EditText>(R.id.etPassword)
+
+        init(v)
         val btn = v.findViewById<View>(R.id.btnRegister) as Button
 
         btn.setOnClickListener {
             requireActivity().run{
+                
+                if (!validate()) {
+                    
+                    return@setOnClickListener
+                }
+                
                 startActivity(Intent(this, MainActivity::class.java))
             }
         }
 
         return v
+    }
+
+
+
+    private fun validate(): Boolean {
+        var isValid = true
+        if (name.text.isBlank()) {
+            name.error = "Name can't be blank!"
+            isValid = false
+        }
+        if (email.text.isBlank()) {
+            email.error = "email can't be blank!"
+            isValid = false
+        }
+        if (password.length() < 8) {
+            password.error = "Password should have should be atleast 8 characters!"
+            isValid = false
+        }
+
+
+        return isValid
+    }
+
+    private fun init(v: View) {
+        name = v.findViewById<EditText>(R.id.etName)
+        email = v.findViewById<EditText>(R.id.etEmail)
+        password = v.findViewById<EditText>(R.id.etPassword)
     }
 
 }
